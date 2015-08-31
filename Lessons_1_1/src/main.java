03e2c5748ebe20b144cacbfecaf5acdcacb8b07a
Taras <taras.sergienko@gmail.com>
2015-08-31 15:41:03 +0300

    Initial commit

---
+++ Lessons_1_1/src/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main {
+    public static void main (String arg []) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println ("Your name");
+        String name = scan.nextLine();
+        System.out.println ( name + "Good");
+
+
+    }
+    }
+
